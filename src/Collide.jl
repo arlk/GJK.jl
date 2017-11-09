c7e9d989abce38c375b61f616afa28c95abe448e
arlk <lakshma2@illinois.edu>
2017-11-09 01:40:46 -0600

    Export gjk

--- src/Collide.jl
+++ src/Collide.jl
@@ -3,6 +3,7 @@ module Collide
 # package code goes here
 function proj(u::AbstractArray{T,1}, v::AbstractArray{T,1}) where {T<:AbstractFloat}
     (u ⋅ v)/(u ⋅ u)*u
+export gjk
 end
 
 include("support.jl")
