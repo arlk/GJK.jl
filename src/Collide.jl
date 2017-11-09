93ae94570c1159c5d10997d99f914ee36b95ae25
arlk <lakshma2@illinois.edu>
2017-11-09 01:43:53 -0600

    Add Result return type + basic refactoring

--- src/Collide.jl
+++ src/Collide.jl
@@ -1,7 +1,16 @@
 module Collide
 
 export gjk
+
+struct Result{T<:AbstractArray{<:AbstractFloat, 2}}
+    status::Bool
+    collision::Bool
+    contact::T
 end
+
+Result() = Result(false, false, Array{Float64, 2}(0, 0))
+Result(res::Bool) = Result(true, res, Array{Float64, 2}(0, 0))
+Result(res::Bool, arr::AbstractArray{<:AbstractFloat, 2}) = Result(true, res, arr)
 
 include("support.jl")
 include("simplex.jl")
--- src/gjk.jl
+++ src/gjk.jl
@@ -1,23 +1,31 @@
 function gjk(p::Any, q::Any, dir::AbstractArray{<:Float64, 1}; tol=1e-10, max_iterations=1000)
-    psimplex = support(p, dir); qsimplex = support(q, -dir); simplex = psimplex - qsimplex
+    psimplex = support(p, dir); qsimplex = support(q, -dir);
+    simplex = psimplex - qsimplex
     dir = -simplex
-    colliding = false
-    closest = [], []
+    result = Result()
 
     for i = 1:max_iterations
-        ps = support(p, dir); qs = support(q, -dir); s = ps - qs
+        ps = support(p, dir); qs = support(q, -dir);
+        s = ps - qs
 
-        if colliding
+        if s ⋅ (-dir) ≥ sum(abs2, dir)*(1.0 - tol) || any(all(simplex .== s, 1))
+            λ = size(simplex, 2) == 1 || findcombination(simplex, -dir)
+            result = Result(false, hcat(psimplex*λ, qsimplex*λ))
             break
-        elseif s ⋅ (-dir) ≥ sum(abs2, dir)*(1.0 - tol) || any(all(simplex .== s, 1))
-            λ = findcombination(simplex, -dir)
-            closest = psimplex*λ, qsimplex*λ
+        end
+
+        psimplex = hcat(psimplex, ps); qsimplex = hcat(qsimplex, qs); simplex = hcat(simplex, s)
+        filtered, dir, collision = findsimplex(simplex)
+        psimplex = psimplex[:, filtered]; qsimplex = qsimplex[:, filtered];
+        simplex = simplex[:, filtered]
+
+        if collision
+            result = Result(true)
             break
-        else
-            psimplex = hcat(psimplex, ps); qsimplex = hcat(qsimplex, qs); simplex = hcat(simplex, s)
-            filtered, dir, colliding = findsimplex(simplex)
-            psimplex = psimplex[:, filtered]; qsimplex = qsimplex[:, filtered]; simplex = simplex[:, filtered]
+        elseif i == max_iterations
+            warn("GJK has not terminated in $max_iterations iterations.")
         end
     end
-    colliding, closest
+
+    result
 end
