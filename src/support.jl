e1e750b1df0abe55d3db16367b237370b4ee7444
arlk <lakshma2@illinois.edu>
2017-11-09 02:30:11 -0600

    Split module imports across functions to for better readability

--- src/support.jl
+++ src/support.jl
@@ -1,17 +1,18 @@
-using GeometryTypes: HyperRectangle, HyperCube, HyperSphere
-
 function support(vertices::AbstractArray{T, 2}, dir::AbstractArray{T, 1}) where {T<:AbstractFloat}
     vertices[:, indmax(vertices'*dir)]
 end
 
+using GeometryTypes: HyperRectangle
 function support(rect::HyperRectangle{N, T}, dir::AbstractArray{T, 1}) where {T<:AbstractFloat, N}
     rect.widths.*normalize(dir, Inf)/2.0 + rect.origin
 end
 
+using GeometryTypes: HyperCube
 function support(cube::HyperCube{N, T}, dir::AbstractArray{T, 1}) where {T<:AbstractFloat, N}
     cube.width*normalize(dir, Inf)/2.0 + cube.origin
 end
 
+using GeometryTypes: HyperSphere
 function support(sphere::HyperSphere{N, T}, dir::AbstractArray{T, 1}) where {T<:AbstractFloat, N}
     sphere.center + sphere.r*dir/norm(dir)
 end
