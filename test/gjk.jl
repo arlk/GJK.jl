052a689d12c041cc891bc083d6af31bc0cf722c7
Arun Lakshmanan <lakshma2@illinois.edu>
2018-02-24 20:42:09 -0600

    Add some 3D tests

--- test/gjk.jl
+++ test/gjk.jl
@@ -1,9 +1,8 @@
 using GeometryTypes: Point, Vec
 using GeometryTypes: HyperSphere, HyperRectangle, HyperCube
 
-@testset "gjk" begin
+@testset "gjk (2D)" begin
     vec2 = [1.0, 0.0]
-    vec3 = [1.0, 0.0, 0.0]
     @testset "array of vertices" begin
         rect2 = [0.0 1.0 1.0 0.0; 0.0 0.0 2.0 2.0; 1.0 1.0 1.0 1.0]
         transform2(θ,x,y) = [cos(θ) -sin(θ) x; sin(θ) cos(θ) y]
@@ -130,3 +129,72 @@ using GeometryTypes: HyperSphere, HyperRectangle, HyperCube
     end # cube
 end # gjk
 
+@testset "gjk (3D)" begin
+    vec3 = [1.0, 0.0, 0.0]
+    @testset "array of vertices" begin
+        # TODO (add several tetrahedron configurations)
+    end # array of vertices
+
+    @testset "sphere" begin
+        sphereA = HyperSphere(Point(0.0, 0.0, 0.0), 1.0)
+        sphereB = HyperSphere(Point(3.0, 3.0, 0.0), 1.0)
+        ret = gjk(sphereA, sphereB, vec3)
+        @test ret.status == true
+        @test ret.collision == false
+        @test all(ret.contact .≈ [0.7071079338737747 2.2928920661262255; 0.7071056284974415 2.2928943715025585; 0.0 0.0])
+
+        sphereA = HyperSphere(Point(0.0, 0.0, 0.0), 1.0)
+        sphereB = HyperSphere(Point(2.0, 0.0, 0.0), 1.0)
+        ret = gjk(sphereA, sphereB, vec3)
+        @test ret.status == true
+        @test ret.collision == true
+
+        sphereA = HyperSphere(Point(0.0, 0.0, 0.0), 1.0)
+        sphereB = HyperSphere(Point(1.0, 0.0, 0.0), 1.0)
+        ret = gjk(sphereA, sphereB, vec3)
+        @test ret.status == true
+        @test ret.collision == true
+    end # sphere
+
+    @testset "rectangle" begin
+        rectangleA = HyperRectangle(Vec(0.0, 0.0, 0.0), Vec(1.0, 2.0, 3.0))
+        rectangleB = HyperRectangle(Vec(3.0, 3.0, 0.0), Vec(1.0, 2.0, 3.0))
+        ret = gjk(rectangleA, rectangleB, vec3)
+        @test ret.status == true
+        @test ret.collision == false
+        @test all(ret.contact .≈ [0.5 2.5; 1.0 2.0; -1.5 -1.5])
+
+        rectangleA = HyperRectangle(Vec(0.0, 0.0, 0.0), Vec(1.0, 2.0, 3.0))
+        rectangleB = HyperRectangle(Vec(1.0, 2.0, 0.0), Vec(1.0, 2.0, 3.0))
+        ret = gjk(rectangleA, rectangleB, vec3)
+        @test ret.status == true
+        @test ret.collision == true
+
+        rectangleA = HyperRectangle(Vec(0.0, 0.0, 0.0), Vec(1.0, 2.0, 3.0))
+        rectangleB = HyperRectangle(Vec(0.5, 0.0, 0.0), Vec(1.0, 2.0, 3.0))
+        ret = gjk(rectangleA, rectangleB, vec3)
+        @test ret.status == true
+        @test ret.collision == true
+    end # rectangle
+
+    @testset "cube" begin
+        cubeA = HyperCube(Vec(0.0, 0.0, 0.0), 1.0)
+        cubeB = HyperCube(Vec(3.0, 3.0, 0.0), 1.0)
+        ret = gjk(cubeA, cubeB, vec3)
+        @test ret.status == true
+        @test ret.collision == false
+        @test all(ret.contact .≈ [0.5 2.5; 0.5 2.5; -0.5 -0.5])
+
+        cubeA = HyperCube(Vec(0.0, 0.0, 0.0), 1.0)
+        cubeB = HyperCube(Vec(1.0, 1.0, 0.0), 1.0)
+        ret = gjk(cubeA, cubeB, vec3)
+        @test ret.status == true
+        @test ret.collision == true
+
+        cubeA = HyperCube(Vec(0.0, 0.0, 0.0), 1.0)
+        cubeB = HyperCube(Vec(0.5, 0.0, 0.0), 1.0)
+        ret = gjk(cubeA, cubeB, vec3)
+        @test ret.status == true
+        @test ret.collision == true
+    end # cube
+end # gjk
