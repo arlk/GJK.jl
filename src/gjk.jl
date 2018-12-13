e4ff314f7ff261fdedded0bcebdbda64809eea3a
Arun Lakshmanan <lakshma2@illinois.edu>
2018-12-12 22:03:47 -0600

    overhaul complete

--- src/gjk.jl
+++ src/gjk.jl
@@ -19,42 +19,63 @@ GJK.Result{Array{Float64,2}}(true, false, [1.0 2.0; 1.0 1.0])
 function gjk(p::Any, q::Any, init_dir::AbstractVector)
     ps = support(p, init_dir)
     qs = support(q, -init_dir)
-    psimplex, qsimplex, dir = init_gjk(ps, qs)
+    psimplex = insertcolumn(ps)
+    qsimplex = insertcolumn(qs)
+    dir = qs - ps
 
-    # Check if we got lucky
-    if isapprox(sum(abs2, dir), 0.0)
-        return true, psimplex, qsimplex
-    end
+    collision, psimplex, qsimplex, dir, sz = _gjk(p, q, psimplex, qsimplex, dir)
 
-    recursive_gjk(p, q, psimplex, qsimplex, dir)
-end
-
-function init_gjk(ps::SVector{N}, qs::SVector{N}) where {N}
-    return SMatrix{N,1}(ps), SMatrix{N,1}(qs), qs - ps
-end
-
-function recursive_gjk(p::Any, q::Any, psimplex::SMatrix, qsimplex::SMatrix, dir::SVector)
-    ps = support(p, dir)
-    qs = support(q, -dir)
-    if (ps - qs) ⋅ (-dir) ≥ sum(abs2, dir) || check_degeneracy(psimplex - qsimplex, ps - qs)
-        pclose, qclose = closestpoints(psimplex, qsimplex, -dir)
-        return false, norm(pclose - qclose), pclose, qclose
-    end
-    psimplex = hcat(psimplex, ps)
-    qsimplex = hcat(qsimplex, qs)
-    npsimplex, nqsimplex, ndir, collision = findsimplex(psimplex, qsimplex)
     if collision
         return true, 0.0, ps, qs
     else
-        return recursive_gjk(p, q, npsimplex, nqsimplex, ndir)
+        pclose, qclose = closestpoints(psimplex, qsimplex, -dir, sz)
+        return false, norm(pclose - qclose), pclose, qclose
     end
 end
 
-@generated function check_degeneracy(simplex::SMatrix{N, M, T}, s::SVector{N, T}) where {N, M, T}
+function _gjk(p::Any, q::Any, psimplex::SMatrix, qsimplex::SMatrix, dir::SVector)
+    sz = 1
+    max_iter = 100
+    collision = false
+
+    if sum(abs2, dir) ≤ 2*eps(Float64)
+        return true, psimplex, qsimplex, dir, sz
+    end
+
+    ps = support(p, dir)
+    qs = support(q, -dir)
+    while check_gjk(psimplex - qsimplex, ps - qs, dir, sz, max_iter)
+        sz += 1
+        psimplex = insertcolumn(psimplex, ps, sz)
+        qsimplex = insertcolumn(qsimplex, qs, sz)
+        psimplex, qsimplex, dir, collision, sz = findsimplex(psimplex, qsimplex, sz)
+        if collision
+            break
+        else
+            ps = support(p, dir)
+            qs = support(q, -dir)
+        end
+
+        max_iter -= 1
+    end
+
+    return collision, psimplex, qsimplex, dir, sz
+end
+
+function check_gjk(simplex::SMatrix, s::SVector, dir::SVector, sz::Int, iter::Int)
+    if iter < 0
+        error("GJK failed: maximum number of iterations reached")
+    elseif s ⋅ (-dir) ≥ sum(abs2, dir) || check_degeneracy(simplex, s, sz)
+        return false
+    else
+        return true
+    end
+end
+
+@generated function check_degeneracy(simplex::SMatrix{N, M, T}, s::SVector{N, T}, sz::Vararg{Int, 1}) where {N, M, T}
     exprs = :(false)
     for i = 1:M
-        releps = :(eps($T)*(1 + sum(abs2, simplex[:, $i]) + sum(abs2, s)))
-        exprs = :($exprs || sum(abs2, simplex[:, $i] - s) ≤ $releps)
+        exprs = :($i > sz[1] ? $exprs : ($exprs || sum(abs2, simplex[:, $i] - s) ≤ 2*eps($T)))
     end
     return exprs
 end
--- src/simplex.jl
+++ src/simplex.jl
@@ -7,29 +7,101 @@ function proj(u::AbstractVector, v::AbstractVector)
     (u ⋅ v)/(u ⋅ u)*u
 end
 
+@generated function insertcolumn(s::SVector{N, T}) where {N, T}
+    S = (N, N+1)
+    exprs = Array{Expr}(undef, S)
+    itr = [1:n for n = S]
+    for k = Base.product(itr...)
+        exprs[k...] = :($(k[2]) > 1 ? zero($T) : s[$(k[1])])
+    end
+
+    return quote
+        Base.@_inline_meta
+        @inbounds elements = tuple($(exprs...))
+        @inbounds return SMatrix{N, N+1, T}(elements)
+    end
+end
+
+@generated function insertcolumn(simplex::SMatrix{N, M, T}, s::SVector{N, T}, idx::Vararg{Int, 1}) where {N, M, T}
+    S = (N, M)
+    exprs = Array{Expr}(undef, S)
+    itr = [1:n for n = S]
+    for k = Base.product(itr...)
+        exprs[k...] = :($(k[2]) > idx[1] ? zero($T) : ($(k[2]) == idx[1] ? s[$(k[1])] : simplex[$(k...)]))
+    end
+
+    return quote
+        Base.@_inline_meta
+        @inbounds elements = tuple($(exprs...))
+        @inbounds return SMatrix{N, M, T}(elements)
+    end
+end
+
+@generated function pickcolumns(simplex::SMatrix{N, M, T}, idx::Vararg{Int, J}) where {N, M, T, J}
+    S = (N, M)
+    exprs = Array{Expr}(undef, S)
+    itr = [1:n for n = S]
+    for k = Base.product(itr...)
+        exprs[k...] = :($(k[2]) > $J ? zero($T) : simplex[$(k[1]), idx[$(k[2])]])
+    end
+
+    return quote
+        Base.@_inline_meta
+        @inbounds elements = tuple($(exprs...))
+        @inbounds return SMatrix{N, M, T}(elements)
+    end
+end
+
+@generated function reducecolumns(simplex::SMatrix{N, M, T}, ::Vararg{Any, J}) where {N, M, T, J}
+    S = (N, J)
+    exprs = Array{Expr}(undef, S)
+    itr = [1:n for n = S]
+    for k = Base.product(itr...)
+        exprs[k...] = :(simplex[$(k...)])
+    end
+
+    return quote
+        Base.@_inline_meta
+        @inbounds elements = tuple($(exprs...))
+        @inbounds return SMatrix{N, J, T}(elements)
+    end
+end
+
 """
     findsimplex(simplex)
 
-Compute the search direction for a given simplex. Return filtered
-indices of the simplex to keep. Return a collision flag true if
-the origin was enclosed by the simplex.
+Compute the new simplices from a pair of given simplices.
+Return the new search direction. Return a collision flag
+if the origin was enclosed by the Minkowski simplex.
 """
-function findsimplex(psimplex::SMatrix{N, 2}, qsimplex::SMatrix{N, 2}) where {N}
+function findsimplex(psimplex::SMatrix, qsimplex::SMatrix, sz::Int)
+    if sz == 2
+        findline(psimplex, qsimplex)
+    elseif sz == 3
+        findtriangle(psimplex, qsimplex)
+    elseif sz == 4
+        findtetrahedron(psimplex, qsimplex)
+    end
+end
+
+function findline(psimplex::SMatrix{N}, qsimplex::SMatrix{N}) where {N}
     simplex = psimplex - qsimplex
     AB = simplex[:, 1] - simplex[:, 2]
     AO = -simplex[:, 2]
     if AB ⋅ AO > 0
         dir = AO - proj(AB, AO)
-        collision = isapprox(sum(abs2, dir), 0.0)
-        return psimplex, qsimplex, dir, collision
+        collision = sum(abs2, dir) ≤ 2*eps(Float64)
+        return psimplex, qsimplex, dir, collision, 2
     else
         dir = AO
-        collision = isapprox(sum(abs2, dir), 0.0)
-        return SMatrix{N, 1}(psimplex[:, 2]), SMatrix{N, 1}(qsimplex[:, 2]), dir, collision
+        psimplex = pickcolumns(psimplex, 2)
+        qsimplex = pickcolumns(qsimplex, 2)
+        collision = sum(abs2, dir) ≤ 2*eps(Float64)
+        return psimplex, qsimplex, dir, collision, 1
     end
 end
 
-function findsimplex(psimplex::SMatrix{N, 3}, qsimplex::SMatrix{N, 3}) where {N}
+function findtriangle(psimplex::SMatrix{N}, qsimplex::SMatrix{N}) where {N}
     simplex = psimplex - qsimplex
     AB = simplex[:, 2] - simplex[:, 3]
     AC = simplex[:, 1] - simplex[:, 3]
@@ -37,41 +109,46 @@ function findsimplex(psimplex::SMatrix{N, 3}, qsimplex::SMatrix{N, 3}) where {N}
     AO = -simplex[:, 3]
     if (AC ⋅ AB * BC - AC ⋅ BC * AB) ⋅ AO > 0
         if AC ⋅ AO > 0
-            idx = SMatrix{3, 2}(1, 0, 0, 0, 0, 1)
+            psimplex = pickcolumns(psimplex, 1, 3)
+            qsimplex = pickcolumns(qsimplex, 1, 3)
             dir = AO - proj(AC, AO)
-            collision = isapprox(sum(abs2, dir), 0.0)
-            return psimplex*idx, qsimplex*idx, dir, collision
+            collision = sum(abs2, dir) ≤ 2*eps(Float64)
+            return psimplex, qsimplex, dir, collision, 2
         else
-            idx = SMatrix{3, 2}(0, 0, 1, 0, 0, 1)
-            return findsimplex(psimplex*idx, qsimplex*idx)
+            psimplex = pickcolumns(psimplex, 2, 3)
+            qsimplex = pickcolumns(qsimplex, 2, 3)
+            return findline(psimplex, qsimplex)
         end
     elseif (AB ⋅ BC * AB - AB ⋅ AB * BC) ⋅ AO > 0
-        idx = SMatrix{3, 2}(0, 0, 1, 0, 0, 1)
-        return findsimplex(psimplex*idx, qsimplex*idx)
+        psimplex = pickcolumns(psimplex, 2, 3)
+        qsimplex = pickcolumns(qsimplex, 2, 3)
+        return findline(psimplex, qsimplex)
     else
-        if isapprox(sum(abs2, AO), 0.0)
-            return psimplex, qsimplex, AO, true
-        elseif isapprox(sum(abs2, AC - proj(AB, AC)), 0.0)
-            idx = SMatrix{3, 2}(0, 0, 1, 0, 0, 1)
-            return findsimplex(psimplex*idx, qsimplex*idx)
+        if sum(abs2, AO) ≤ 2*eps(Float64)
+            return psimplex, qsimplex, AO, true, 3
+        elseif sum(abs2, AC - proj(AB, AC)) ≤ 2*eps(Float64)
+            psimplex = pickcolumns(psimplex, 2, 3)
+            qsimplex = pickcolumns(qsimplex, 2, 3)
+            return findline(psimplex, qsimplex)
         elseif N == 2
-            return psimplex, qsimplex, AO, true
+            return psimplex, qsimplex, AO, true, 3
         else
             ABC = AB × BC
             dir = proj(ABC, AO)
             if ABC ⋅ AO > 0
-                idx = SMatrix{3, 3}(0, 1, 0, 1, 0, 0, 0, 0, 1)
-                collision = isapprox(sum(abs2, dir), 0.0)
-                return psimplex*idx, qsimplex*idx, dir, collision
+                psimplex = pickcolumns(psimplex, 2, 1, 3)
+                qsimplex = pickcolumns(qsimplex, 2, 1, 3)
+                collision = sum(abs2, dir) ≤ 2*eps(Float64)
+                return psimplex, qsimplex, dir, collision, 3
             else
-                collision = isapprox(sum(abs2, dir), 0.0)
-                return psimplex, qsimplex, dir, collision
+                collision = sum(abs2, dir) ≤ 2*eps(Float64)
+                return psimplex, qsimplex, dir, collision, 3
             end
         end
     end
 end
 
-function findsimplex(psimplex::SMatrix{N, 4}, qsimplex::SMatrix{N, 4}) where {N}
+function findtetrahedron(psimplex::SMatrix{N}, qsimplex::SMatrix{N}) where {N}
     simplex = psimplex - qsimplex
     AB = simplex[:, 3] - simplex[:, 4]
     AC = simplex[:, 2] - simplex[:, 4]
@@ -80,44 +157,51 @@ function findsimplex(psimplex::SMatrix{N, 4}, qsimplex::SMatrix{N, 4}) where {N}
     BD = simplex[:, 1] - simplex[:, 3]
     AO = -simplex[:, 4]
     if (AB × AC) ⋅ AO < 0
-        idx = SMatrix{4, 3}(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-        return findsimplex(psimplex*idx, qsimplex*idx)
+        psimplex = pickcolumns(psimplex, 2, 3, 4)
+        qsimplex = pickcolumns(qsimplex, 2, 3, 4)
+        return findtriangle(psimplex, qsimplex)
     elseif (AD × AB) ⋅ AO < 0
-        idx = SMatrix{4, 3}(0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
-        return findsimplex(psimplex*idx, qsimplex*idx)
+        psimplex = pickcolumns(psimplex, 2, 3, 4)
+        qsimplex = pickcolumns(qsimplex, 2, 3, 4)
+        return findtriangle(psimplex, qsimplex)
     elseif (AC × AD) ⋅ AO < 0
-        idx = @SMatrix zeros(M, M-1)
-        idx = SMatrix{4, 3}(1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1)
-        return findsimplex(psimplex*idx, qsimplex*idx)
+        psimplex = pickcolumns(psimplex, 2, 3, 4)
+        qsimplex = pickcolumns(qsimplex, 2, 3, 4)
+        return findtriangle(psimplex, qsimplex)
     else
-        return psimplex, qsimplex, AO, true
+        return psimplex, qsimplex, AO, true, 4
     end
 end
 
 """
-    findcombination(psimplex, qsimplex, vec)
+    closestpoints(psimplex, qsimplex, dir2origin)
 
-Compute the coefficients of a convex combination of the given
-simplex vertices that equals the vector `vec`. Project `vec`
-onto the closest vertex or edge of the simplex if no solution
-is available.
+Compute the closest points between two simplexes given the
+direction to in origin in the Minkowski difference space
 """
-function closestpoints(psimplex::SMatrix{N, M}, qsimplex::SMatrix{N, M}, dir2origin::SVector{N}) where {N, M}
-    λ = SVector{M}(cvxcombination(psimplex - qsimplex, dir2origin))
-    return psimplex*λ, qsimplex*λ
+function closestpoints(psimplex::SMatrix{N, M}, qsimplex::SMatrix{N, M}, dir2origin::SVector{N}, sz::Int) where {N, M}
+    if sz == 1
+        return psimplex[:, 1], qsimplex[:, 1]
+    elseif sz == 2
+        λ = linecombination(psimplex - qsimplex, dir2origin)
+        pclose = reducecolumns(psimplex, λ...)*SVector{2}(λ)
+        qclose = reducecolumns(qsimplex, λ...)*SVector{2}(λ)
+        return pclose, qclose
+    elseif sz == 3
+        λ = trianglecombination(psimplex - qsimplex, dir2origin)
+        pclose = reducecolumns(psimplex, λ...)*SVector{3}(λ)
+        qclose = reducecolumns(qsimplex, λ...)*SVector{3}(λ)
+        return pclose, qclose
+    end
 end
 
 """
-    findlinecombination(simplex, vec)
+    cvxcombination(simplex, vec)
 
-Called by `findcombination()` to solve the convex combination
-problem for a line simplex defined by two points.
+Called by `closestpoints()` to solve the convex combination
+problem for a line or triangle simplex defined by two points.
 """
-function cvxcombination(simplex::SMatrix{N, 1}, vec::SVector{N}) where {N}
-    return 1.0
-end
-
-function cvxcombination(simplex::SMatrix{N, 2}, vec::SVector{N}) where {N}
+function linecombination(simplex::SMatrix{N}, vec::SVector{N}) where {N}
     AV = vec - simplex[:, 2]
     AB = simplex[:, 1] - simplex[:, 2]
     λ = (AB ⋅ AV)/(AB ⋅ AB)
@@ -128,7 +212,7 @@ function cvxcombination(simplex::SMatrix{N, 2}, vec::SVector{N}) where {N}
     end
 end
 
-function cvxcombination(simplex::SMatrix{N, 3}, vec::SVector{N}) where {N}
+function trianglecombination(simplex::SMatrix{N}, vec::SVector{N}) where {N}
     AO = -simplex[:, 3]
     AV = vec - simplex[:, 3]
     AB = simplex[:, 2] - simplex[:, 3]
@@ -139,18 +223,19 @@ function cvxcombination(simplex::SMatrix{N, 3}, vec::SVector{N}) where {N}
         if AC ⋅ AV > 0
             dir = AO - proj(AC, AV)
             idx = SMatrix{3, 2}(1, 0, 0, 0, 0, 1)
-            λAC1, λAC2 = cvxcombination(simplex*idx, -dir)
+            simplex = pickcolumns(simplex, 1, 3)
+            λAC1, λAC2 = cvxcombination(simplex, -dir)
             return λAC1, 0.0, λAC2
         else
             dir = AO - proj(AB, AV)
-            idx = SMatrix{3, 2}(0, 0, 1, 0, 0, 1)
-            λAB1, λAB2 = cvxcombination(simplex*idx, -dir)
+            simplex = pickcolumns(simplex, 2, 3)
+            λAB1, λAB2 = cvxcombination(simplex, -dir)
             return 0.0, λAB1, λAB2
         end
     elseif (AB ⋅ BC * AB - AB ⋅ AB * BC) ⋅ AV > 0
         dir = AO - proj(AB, AV)
-        idx = SMatrix{3, 2}(0, 0, 1, 0, 0, 1)
-        λAB1, λAB2 = cvxcombination(simplex*idx, -dir)
+        simplex = pickcolumns(simplex, 2, 3)
+        λAB1, λAB2 = cvxcombination(simplex, -dir)
         return 0.0, λAB1, λAB2
     else
         sABC = [AC AB]
